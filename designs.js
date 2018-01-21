983c4b8be9b44f15b153a84a98a443f9a2591131
Amanda Reilly <amandamichellereilly@gmail.com>
2018-01-20 21:44:26 -0800

    Add makeGrid function and event listener

--- designs.js
+++ designs.js
@@ -1,10 +1,27 @@
-// Select color input
-// Select size input
+// makes a grid of cells based on user input
+function makeGrid(event) {
+  event.preventDefault();
+  const pixelCanvas = $("#pixel_canvas");
+  const gridHeight = $("#input_height").val();
+  const gridWidth = $("#input_width").val();
+  let row = "";
+  let table = "";
+  //create a row of gridWidth cells
+  for(let i = 0; i < gridWidth; i++) {
+    row += "<td></td>";
+  }
 
-// When size is submitted by the user, call makeGrid()
+  //create a table of gridHeight rows of cells
+  for(let j = 0; j < gridHeight; j++) {
+    table += "<tr>" + row + "</tr>";
+  }
 
-function makeGrid() {
+  //empty existing table
+  pixelCanvas.empty();
 
-// Your code goes here!
-
+  //add table to pixelCanvas
+  pixelCanvas.append(table);
 }
+
+//event listener for grid size submit
+$("#submit_size").click(makeGrid);
